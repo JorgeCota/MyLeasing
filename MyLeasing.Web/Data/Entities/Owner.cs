4673cb81be7e5bce824a83b4d3803d93cd41f8b6
jorgeluiscotaanduaga <jorgeluiscotaanduaga@outlook.com>
2019-08-03 17:25:18 -0600

    BaseDatosUpdate

--- MyLeasing.Web/Data/Entities/Owner.cs
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,34 @@ namespace MyLeasing.Web.Data.Entities
 {
     public class Owner
     {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Document { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [MaxLength(20)]
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+        [MaxLength(20)]
+        [Display(Name = "Cell Phone")]
+        public string CellPhone { get; set; }
+
+        public string Address { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string FullNameWhitDocument => $"{FirstName} {LastName} - {Document}";
+
     }
 }
