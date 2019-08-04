a76c0bf4eb83bfbd1496e3bbf410e65082545610
jorgeluiscotaanduaga <jorgeluiscotaanduaga@outlook.com>
2019-08-04 15:29:24 -0600

    DB complete

--- MyLeasing.Web/Data/DataContext.cs
+++ MyLeasing.Web/Data/DataContext.cs
@@ -11,5 +11,11 @@ namespace MyLeasing.Web.Data
         }
 
         public DbSet<Owner> Owners { get; set; }
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<Lessee> Lessees { get; set; }
+        public DbSet<Property> Properties { get; set; }
+        public DbSet<PropertyImage> PropertyImages { get; set; }
+        public DbSet<PropertyType> PropertyTypes { get; set; }
+
     }
 }
